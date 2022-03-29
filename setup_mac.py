ecbdb5465e95784240c0ef035d5b565163afe930
sunjit22 <63877211+sunjit22@users.noreply.github.com>
2022-03-29 11:55:26 -0400

    Mac env

---
+++ setup_mac.py
@@ -0,0 +1,70 @@
+import os
+import pathlib
+import platform
+
+import pkg_resources
+from setuptools import find_packages, setup
+
+# Basic Information
+NAME = "inpaint"
+VERSION = "0.1.0"
+DESCRIPTION = "A python package for image inpainting"
+REPOSITORY = "https://github.com/prajnan93/image-inpainting"
+
+# Define the classifiers
+# See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+CLASSIFIERS = [
+    "Development Status :: 2 - Pre-Alpha",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "Programming Language :: Python :: 3.8",
+]
+
+
+# Define the keywords
+KEYWORDS = [
+    "pytorch",
+    "machine learning",
+    "deep learning",
+    "image inpainting",
+    "computer vision",
+]
+
+# Directories to ignore in find_packages
+EXCLUDES = ()
+
+# Important Paths
+PROJECT = os.path.abspath(os.path.dirname(__file__))
+
+if platform.system().lower() == "windows":
+    REQUIRE_PATH = "requirements.txt"
+else:
+    REQUIRE_PATH = "requirements_mac.txt"
+
+
+with pathlib.Path(REQUIRE_PATH).open() as requirements_txt:
+    INSTALL_REQUIRES = [
+        str(requirement)
+        for requirement in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+
+CONFIG = {
+    "name": NAME,
+    "version": VERSION,
+    "description": DESCRIPTION,
+    "classifiers": CLASSIFIERS,
+    "keywords": KEYWORDS,
+    "url": REPOSITORY,
+    "packages": find_packages(
+        where=PROJECT, include=["inpaint", "inpaint.*"], exclude=EXCLUDES
+    ),
+    "install_requires": INSTALL_REQUIRES,
+    "python_requires": ">=3.6",
+    "test_suite": "tests",
+    "tests_require": ["pytest>=3"],
+    "include_package_data": True,
+}
+
+if __name__ == "__main__":
+    setup(**CONFIG)
