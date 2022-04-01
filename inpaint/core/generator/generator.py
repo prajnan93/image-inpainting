3c0c650afbfa7bd29cbc43f3ae2276ed09f3125d
sunjit22 <63877211+sunjit22@users.noreply.github.com>
2022-03-31 20:14:00 -0400

    Adding generator

---
+++ inpaint/core/generator/generator.py
@@ -0,0 +1,485 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+
+from inpaint.core.modules import GatedConv2d, TransposeGatedConv2d
+
+
+# -----------------------------------------------
+#                   Generator
+# -----------------------------------------------
+# Input: masked image + mask
+# Output: filled image
+class GatedGenerator(nn.Module):
+    def __init__(self, opt):
+        super(GatedGenerator, self).__init__()
+        self.coarse = nn.Sequential(
+            # encoder
+            GatedConv2d(
+                opt.in_channels,
+                opt.latent_channels,
+                5,
+                1,
+                2,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels,
+                opt.latent_channels * 2,
+                3,
+                2,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 2,
+                opt.latent_channels * 2,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 2,
+                opt.latent_channels * 4,
+                3,
+                2,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            # Bottleneck
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                2,
+                dilation=2,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                4,
+                dilation=4,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                8,
+                dilation=8,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                16,
+                dilation=16,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            # decoder
+            TransposeGatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 2,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 2,
+                opt.latent_channels * 2,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            TransposeGatedConv2d(
+                opt.latent_channels * 2,
+                opt.latent_channels,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels,
+                opt.latent_channels // 2,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels // 2,
+                opt.out_channels,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation="none",
+                norm=opt.norm,
+            ),
+            nn.Tanh(),
+        )
+
+        self.refine_conv = nn.Sequential(
+            GatedConv2d(
+                opt.in_channels,
+                opt.latent_channels,
+                5,
+                1,
+                2,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels,
+                opt.latent_channels,
+                3,
+                2,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels,
+                opt.latent_channels * 2,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 2,
+                opt.latent_channels * 2,
+                3,
+                2,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 2,
+                opt.latent_channels * 4,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                2,
+                dilation=2,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                4,
+                dilation=4,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                8,
+                dilation=8,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                16,
+                dilation=16,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+        )
+        self.refine_atten_1 = nn.Sequential(
+            GatedConv2d(
+                opt.in_channels,
+                opt.latent_channels,
+                5,
+                1,
+                2,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels,
+                opt.latent_channels,
+                3,
+                2,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels,
+                opt.latent_channels * 2,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 2,
+                opt.latent_channels * 4,
+                3,
+                2,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation="relu",
+                norm=opt.norm,
+            ),
+        )
+        self.refine_atten_2 = nn.Sequential(
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+        )
+        self.refine_combine = nn.Sequential(
+            GatedConv2d(
+                opt.latent_channels * 8,
+                opt.latent_channels * 4,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 4,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            TransposeGatedConv2d(
+                opt.latent_channels * 4,
+                opt.latent_channels * 2,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels * 2,
+                opt.latent_channels * 2,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            TransposeGatedConv2d(
+                opt.latent_channels * 2,
+                opt.latent_channels,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels,
+                opt.latent_channels // 2,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation=opt.activation,
+                norm=opt.norm,
+            ),
+            GatedConv2d(
+                opt.latent_channels // 2,
+                opt.out_channels,
+                3,
+                1,
+                1,
+                pad_type=opt.pad_type,
+                activation="none",
+                norm=opt.norm,
+            ),
+            nn.Tanh(),
+        )
+        self.context_attention = ContextualAttention(
+            ksize=3, stride=1, rate=2, fuse_k=3, softmax_scale=10, fuse=True
+        )
+
+    def forward(self, img, mask):
+        # img: entire img
+        # mask: 1 for mask region; 0 for unmask region
+        # Coarse
+        first_masked_img = img * (1 - mask) + mask
+        first_in = torch.cat((first_masked_img, mask), dim=1)  # in: [B, 4, H, W]
+        first_out = self.coarse(first_in)  # out: [B, 3, H, W]
+        first_out = nn.functional.interpolate(first_out, (img.shape[2], img.shape[3]))
+        # Refinement
+        second_masked_img = img * (1 - mask) + first_out * mask
+        second_in = torch.cat([second_masked_img, mask], dim=1)
+        refine_conv = self.refine_conv(second_in)
+        refine_atten = self.refine_atten_1(second_in)
+        mask_s = nn.functional.interpolate(
+            mask, (refine_atten.shape[2], refine_atten.shape[3])
+        )
+        refine_atten = self.context_attention(refine_atten, refine_atten, mask_s)
+        refine_atten = self.refine_atten_2(refine_atten)
+        second_out = torch.cat([refine_conv, refine_atten], dim=1)
+        second_out = self.refine_combine(second_out)
+        second_out = nn.functional.interpolate(second_out, (img.shape[2], img.shape[3]))
+        return first_out, second_out
