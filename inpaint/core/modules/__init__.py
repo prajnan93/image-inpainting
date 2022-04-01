373fe6ecd2438123ee8a77ac33c01b74e1047355
sunjit22 <63877211+sunjit22@users.noreply.github.com>
2022-03-31 21:28:57 -0400

    Initalise modules

--- inpaint/core/modules/__init__.py
+++ inpaint/core/modules/__init__.py
@@ -1,4 +1,4 @@
 from .attention import ContextualAttention
 from .conv import Conv2dLayer, GatedConv2d, TransposeConv2dLayer, TransposeGatedConv2d
-from .percetual_net import PerceptualNet
+from .perceptual_net import PerceptualNet
 from .weights import weights_init
