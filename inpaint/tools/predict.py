7337c8725af83e5b34a9ed69015d3325225794f1
prajnan93 <prajnan.videoworks@gmail.com>
2022-04-20 13:52:06 -0400

    updated notebooks

--- inpaint/tools/predict.py
+++ inpaint/tools/predict.py
@@ -1,9 +1,10 @@
-import numpy as np
 import cv2
+import numpy as np
 import torch
 import torchvision.transforms as transforms
 
 from inpaint.utils import denorm
+
 
 def predict(generator, img, mask):
 
@@ -21,18 +22,17 @@ def predict(generator, img, mask):
     img = img.to(device)
     mask = mask.to(device)
 
-    normalize = transforms.Compose([
-        transforms.Normalize(
-            mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]
-        )
-    ])
+    normalize = transforms.Compose(
+        [transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])]
+    )
 
     img = normalize(img)
-    
+
     img = img.unsqueeze(0)
     mask = mask.unsqueeze(0)
 
-    coarse_output, refine_output = generator(img, mask)
+    with torch.no_grad():
+        coarse_output, refine_output = generator(img, mask)
 
     refine_output = denorm(refine_output)
 
@@ -40,6 +40,5 @@ def predict(generator, img, mask):
     result = cv2.convertScaleAbs(result, alpha=(255.0))
     result = np.clip(result, 0, 255)
     result = result.astype(np.uint8)
-    result = cv2.cvtColor(result, cv2.COLOR_RGB2BGR)
 
     return result
